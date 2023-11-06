df386336c2e9dbff03a6d02ed7e52b53b4ea3f56
Faez <faez@psight.io>
2023-11-06 17:32:20 +0400

    Adding products links to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { cookies } from "next/headers";
 import { getCart, getItems } from "@/utils/prisma-actions";
 import ProductPic from "@/public/product-pic.jpg";
 import axios from "axios";
+import Link from "next/link";
 
 const products = [
   {
@@ -19,7 +20,7 @@ const products = [
 const prisma = new PrismaClient();
 
 export default async function Home() {
-  let cartId = cookies().get("cartId")?.value;
+  // let cartId = cookies().get("cartId")?.value;
 
   // let products = await axios.get("http://127.0.0.1:1337/api/products", {
   //   params: {
@@ -34,25 +35,16 @@ export default async function Home() {
   //   // },
   // });
 
-  const items = await getItems();
-  const cartItems = await getCart(cartId);
+  // const items = await getItems();
+  // const cartItems = await getCart(cartId);
 
   return (
     <div className="mx-5">
-      <h1 className="">Best Seller</h1>
-
-      <p>Elavate your beauty routine with our all-time fan favorites.</p>
-
-      <div className="flex gap-2 my-2">
-        <button className="px-4 py-2">Sort by</button>
-        <button className="px-4 py-2">Filter</button>
-      </div>
-
-      <div className="grid md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 justify-center gap-4">
-        {products.map((item) => (
-          <Product key={item.id} data={item} />
-        ))}
-      </div>
+      <Link href="/products">
+        <button>
+          <h1 className="text-3xl font-bold">Products</h1>
+        </button>
+      </Link>
     </div>
   );
 }
